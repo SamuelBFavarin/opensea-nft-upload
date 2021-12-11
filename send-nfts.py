ce1a174300a5fa3d38060fe4949d5c7fd043e2e7
SamuelBFavarin <samuelbfavarin@hotmail.com>
2021-12-11 15:54:11 -0300

    feat: opensea login

--- send-nfts.py
+++ send-nfts.py
@@ -8,18 +8,18 @@ EXTENSION_PATH='assets/metamask.crx'
 EXECUTABLE_PATH='assets/chromedriver'
 
 
-def create_webdrive_session():
+def create_webdrive_session() -> webdriver:
     opt = webdriver.ChromeOptions()
     opt.add_extension(EXTENSION_PATH)
 
     return webdriver.Chrome(executable_path=EXECUTABLE_PATH,
                             options=opt)
 
-def get_login_config():
+def get_config() -> json:
     with open('config.json', 'r') as openfile:
         return json.load(openfile)
 
-def login_meta_mask(driver, credentials):
+def login_meta_mask(driver:webdriver, credentials:json) -> bool:
 
     try:
         time.sleep(5)
@@ -49,10 +49,40 @@ def login_meta_mask(driver, credentials):
         logging.error("Error on logging in metamask")
         return False
 
+def login_opensea(driver:webdriver, credentials:json) -> bool:
+    
+    try:
+        opensea_url = f"""https://opensea.io/collection/{credentials["collection_name"]}/assets/create"""
+        driver.execute_script(f"""window.open('{opensea_url}', '_blank')""")
+        driver.switch_to.window(driver.window_handles[2])
+        time.sleep(7)
+
+        driver.find_element(By.XPATH, '//*[@id="main"]/div/div/div/div[2]/ul/li[1]/button').click()
+        time.sleep(2)
+
+        driver.switch_to.window(driver.window_handles[3])
+        time.sleep(3)
+
+        driver.find_element(By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div[2]/div[4]/div[2]/button[2]').click()
+        time.sleep(3)
+
+        driver.switch_to.window(driver.window_handles[3])
+        driver.find_element(By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div[2]/div[2]/div[2]/footer/button[2]').click()
+        time.sleep(3)
+
+        driver.switch_to.window(driver.window_handles[3])
+        driver.find_element(By.XPATH, '//*[@id="app-content"]/div/div[2]/div/div[3]/button[2]').click()
+        time.sleep(5)
+    except Exception as e:
+        logging.error(e)
+        logging.error("Error on logging in opensea")
+        return False
 
 if __name__ == '__main__':
     driver = create_webdrive_session()
-    login_config = get_login_config()
+    config = get_config()
 
-    login_meta_mask(driver, login_config['meta_mask_credentials'])
+    if login_meta_mask(driver, config['meta_mask_credentials']):
+        if login_opensea(driver, config['opensea_credentials']):
+            pass
 
